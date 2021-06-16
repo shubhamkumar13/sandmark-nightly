405b5c438d5e9d14e6b32f8df1b645b79760cc5a
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-06-16 13:11:40 +0530

    de-duplicate nightly bash script

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -7,6 +7,7 @@ TOKEN=${TOKEN:-""}
 
 #git urls of ocaml 4.12.0, ocaml 4.12+domains+effects, ocaml 4.12+domains
 OCAML_412_DOMAINS_EFFECTS="https://github.com/ocaml-multicore/ocaml-multicore.git refs/heads/4.12+domains+effects"
+OCAML_412_STOCK="https://github.com/ocaml/ocaml.git refs/heads/4.12"
 
 #replace urls with latest commit id
 function get_latest_commit {
@@ -15,6 +16,7 @@ function get_latest_commit {
 }
 
 OCAML_412_DOMAINS_EFFECTS=$(get_latest_commit "git ls-remote $OCAML_412_DOMAINS_EFFECTS")
+OCAML_412_STOCK=$(get_latest_commit "git ls-remote $OCAML_412_STOCK")
 
 #check if sandmark and sandmark-nightly repo exist in the default or custom SANDMARK_NIGHTLY_DIR
 function check_sandmark_subdir {
@@ -37,6 +39,7 @@ check_sandmark_subdir $SANDMARK_NIGHTLY_DIR
 #initialize the date and time and create sequential and parallel directories
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 mkdir -p $SANDMARK_NIGHTLY_DIR/sandmark-nightly/sequential/$TIMESTAMP/$OCAML_412_DOMAINS_EFFECTS
+mkdir -p $SANDMARK_NIGHTLY_DIR/sandmark-nightly/sequential/$TIMESTAMP/$OCAML_412_STOCK
 mkdir -p $SANDMARK_NIGHTLY_DIR/sandmark-nightly/parallel/$TIMESTAMP/$OCAML_412_DOMAINS_EFFECTS
 
 #get the latest sandmark pull
@@ -50,22 +53,19 @@ TAG='"macro_bench"' make run_config_filtered.json
 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+stock.bench
 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains+effects.bench
 
-cp -a $SANDMARK_NIGHTLY_DIR/sandmark/_results/*.bench $SANDMARK_NIGHTLY_DIR/sandmark-nightly/sequential/$TIMESTAMP/$OCAML_412_DOMAINS_EFFECTS
+cp -a $SANDMARK_NIGHTLY_DIR/sandmark/_results/4.12.0+domains+effects_1.orun.summary.bench $SANDMARK_NIGHTLY_DIR/sandmark-nightly/sequential/$TIMESTAMP/$OCAML_412_DOMAINS_EFFECTS
+cp -a $SANDMARK_NIGHTLY_DIR/sandmark/_results/4.12.0+stock_1.orun.summary.bench $SANDMARK_NIGHTLY_DIR/sandmark-nightly/sequential/$TIMESTAMP/$OCAML_412_STOCK
 rm -rf _results/
 
 #parallel benchmarks
 TAG='"macro_bench"' make multicore_parallel_run_config_filtered.json
-RUN_BENCH_TARGET=run_orunchrt \
-	BUILD_BENCH_TARGET=multibench_parallel \
-	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
-	make ocaml-versions/4.12.0+domains.bench
 
 RUN_BENCH_TARGET=run_orunchrt \
 	BUILD_BENCH_TARGET=multibench_parallel \
 	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
 	make ocaml-versions/4.12.0+domains+effects.bench
 
-cp -a $SANDMARK_NIGHTLY_DIR/sandmark/_results/*.bench $SANDMARK_NIGHTLY_DIR/sandmark-nightly/parallel/$TIMESTAMP/$OCAML_412_DOMAINS_EFFECTS
+cp -a $SANDMARK_NIGHTLY_DIR/sandmark/_results/4.12.0+domains+effects_1.orunchrt.summary.bench $SANDMARK_NIGHTLY_DIR/sandmark-nightly/parallel/$TIMESTAMP/$OCAML_412_DOMAINS_EFFECTS
 rm -rf _results/
 
 #push to sandmark-nightly
